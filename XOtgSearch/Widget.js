92db16016124466ba96bd29f3f12dca9f77f0a4c
zsoft <zsoft@bk.rr>
2022-11-08 17:03:29 +0200

    XOtgSearch

---
+++ XOtgSearch/Widget.js
@@ -0,0 +1,324 @@
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+define(['dojo/_base/declare', 'jimu/BaseWidget'
+  , "esri/request", "esri/tasks/Geoprocessor", "esri/tasks/DataFile",
+    "esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol",
+    "esri/renderers/SimpleRenderer",
+    "esri/Color",
+
+    "esri/InfoTemplate",
+
+    "esri/geometry/Polygon",
+    "esri/graphic",
+    "esri/graphicsUtils",
+
+    "esri/SpatialReference",
+    "esri/geometry/projection",
+
+    "esri/toolbars/draw",
+    'jimu/dijit/DrawBox',
+    "esri/symbols/SimpleFillSymbol",
+    'esri/layers/GraphicsLayer',
+    'esri/layers/FeatureLayer',
+    // 'dojo/number',
+    'dojo/i18n',
+    'dojo/i18n!esri/nls/jsapi',
+    'dojo/_base/html',
+    'dojo/_base/lang',
+
+    "dojo/on",
+    "dojo/_base/array",
+    "dojo/dom",
+    "dojo/domReady!"
+],
+    function (declare, BaseWidget
+        , esriRequest, Geoprocessor, DataFile,
+        SimpleLineSymbol, SimpleFillSymbol, SimpleRenderer, Color,
+        InfoTemplate,
+
+        Polygon,
+        Graphic,
+        graphicsUtils,
+        SpatialReference, projection,
+
+        Draw, DrawBox,
+        SimpleFillSymbol,
+        GraphicsLayer, FeatureLayer,
+        dojoI18n, esriNlsBundle,
+        html, lang,
+
+
+        on, array, dom
+    ) {
+        //To create a widget, you need to derive from BaseWidget.
+        return declare([BaseWidget], {
+            // Custom widget code goes here
+            baseClass: 'jimu-widget-xotgparcel',
+            _defaultGsUrl: 
+        		'//tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer',
+            // TODO: own GeometryServer from config
+            _undoManager: null,
+            _graphicsLayer: null,
+            _objectIdCounter: 1,
+            _objectIdName: 'OBJECTID',
+            _objectIdType: 'esriFieldTypeOID',
+            _polygonLayer: null,
+            _labelLayer: null,
+            drawtoolbar: null,
+            //_drawtoolbar: new Draw(this.map),
+            _dt: null,
+            _symPoly: null,
+            urlParcelService: 'http://192.168.0.115:5020/parcelgeom/',
+            _gs: 'http://192.168.0.115:6080/arcgis/rest/services/Geometry/GeometryServer',
+                //null, // own geometry service
+            dtbox: '',
+
+
+            //this property is set by the framework when widget is loaded.
+            name: 'XOtgParcel',
+            //methods to communication with app container:
+
+            
+            postMixInProperties: function () {
+                this.inherited(arguments);
+                this.jimuNls = window.jimuNls;
+                //this.config.isOperationalLayer = !!this.config.isOperationalLayer;
+                //point locale decimal
+                //this.numberDecimal = dojoI18n.getLocalization("dojo.cldr", "number", window.dojoConfig.locale).decimal;
+                console.log(esriConfig.defaults.geometryService)
+                if (esriConfig.defaults.geometryService) {
+                    this._gs = esriConfig.defaults.geometryService;
+                } else {
+                    this._gs = new GeometryService(this._defaultGsUrl);
+                }
+    
+            },
+            
+            postCreate: function () {
+                this.inherited(arguments);
+                console.log('postCreate');
+                //this._initGraphicsLayers();
+            },
+
+    
+            _initGraphicsLayers: function () {
+                this._graphicsLayer = new GraphicsLayer();
+
+                // if (!this.config.isOperationalLayer) {
+                    //this._polygonLayer = new GraphicsLayer();
+                    //this.map.addLayer(this._polygonLayer);
+                    //this.map.addLayer(this._labelLayer);
+                // }
+            },
+
+            _removeEmptyLayers: function () {
+                //if (this._polygonLayer && this._polygonLayer.graphics.length === 0) {
+                //    this.map.removeLayer(this._polygonLayer);
+                //    this._polygonLayer = null;
+                //}
+                },
+             startDraw: function () {
+                this.map.graphics.clear();
+             },
+
+            _onBtnPolygonClick: function () {
+                this.map.graphics.clear();
+
+
+                //this.drawtoolbar = new Draw(this.map)
+                //this.drawtoolbar.on("draw-end", this._addToMap)
+                
+                window.__mg_drawtoolbar = this.drawtoolbar;
+                this.map.setInfoWindowOnClick(false);
+                this.drawtoolbar.activate('polygon');  //'polygon' Draw['POLYGON']
+                this.map.hideZoomSlider();
+                
+                //console.log('polygon');
+            },
+
+
+            _onBtnClearClick: function () {
+                this.map.graphics.clear();
+                dom.byId('message').innerHTML = "";
+                
+                // var map = this.map;
+                // var srMap = map.extent.spatialReference;
+                
+                  // var myPolygon = {"geometry":{"rings":[[[59536.878738427935,27053.246601985968],[59565.98296330305,27041.075744310918],[59555.002733009256,27020.570494967087],[59531.58706117791,27030.360097879624],[59536.878738427935,27053.246601985968]]],
+                    // "spatialReference":srMap},
+                    // "symbol":{"color":[0,0,0,64],"outline":{"color":[0,0,0,255],
+                    // "width":1,"type":"esriSLS","style":"esriSLSSolid"},
+                    // "type":"esriSFS","style":"esriSFSSolid"}};
+                  
+                  // var gra = new Graphic(myPolygon);
+                
+                    // map.graphics.add(gra);
+                    // try {
+                      // var extent = graphicsUtils.graphicsExtent([gra]).expand(1.2);
+                      // map.setExtent(extent);
+                    // } catch (err) {
+                      // console.log(err)
+                    // }
+                
+            },
+
+            /*
+            _onDrawEnd: function (graphic, geotype, commontype) {
+            //jshint unused: false
+            this.drawBox.clear();
+            if (!graphic.symbol) { //not draw and save graphic that has null symbol.
+                return;
+            }
+
+            var geometry = graphic.geometry;
+            console.log(geometry);
+
+             },
+            */
+
+            addToMap: function (evt) {
+
+                function showPopUp(url, parameters) {
+                    popUpObj = window.open(url,
+                        "ModalPopUp",
+                        "popup=yes," +
+                        "toolbar=no," +
+                        "scrollbars=no," +
+                        "location=no," +
+                        "statusbar=no," +
+                        "menubar=no," +
+                        "resizable=0," +
+                        "width=700," +
+                        "height=500," +
+                        "left = 490," +
+                        "top=100");
+
+                    // popUpObj.focus();
+
+                }
+
+                var symbol;
+                this.map.showZoomSlider();
+
+                //this.drawtoolbar.deactivate(); //  null ? 
+
+                symbol = new SimpleFillSymbol();
+
+                this._symPoly = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+                    new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT,
+                        new Color([255, 0, 0]), 3), new Color([255, 255, 0, 0.1]));
+
+                //var graphic = new Graphic(evt.geometry, symbol);
+                var graphic = new Graphic(evt.geometry, this._symPoly);
+                this.map.graphics.add(graphic);
+
+                geojson0 = '{"type": "POLYGON", "coordinates":' + JSON.stringify(graphic.geometry.rings) + '}'
+                //geojson = '{"type": "POLYGON", "coordinates":' + JSON.stringify(graphic.geometry.rings) +
+                //    ',"crs":{"type":"name","properties":{"name":"ESRI:' + graphic.geometry.spatialReference.wkid + '"}'
+                //geojson3857 = '{"type": "POLYGON", "coordinates":' + JSON.stringify(graphic.geometry.rings) +
+                //    ',"crs":{"type":"name","properties":{"name":"EPSG:3857"}'
+
+                console.log(geojson0)
+                // select st_geomfromgeojson(geojson) 
+                dom.byId('message').innerHTML = geojson0;
+                showPopUp('http://192.168.17.45:5024/parcelgeom/' + geojson0);
+                //showPopUp(this._urlParcelService + geojson0);
+                //showPopUp('http://192.168.0.115:5020/parcel/2323981500010010105');
+                
+                window.__mg_drawtoolbar.deactivate();
+                this.map.setInfoWindowOnClick(true);
+            },
+            /*
+            _showPopUp: function (url, parameters) {
+                popUpObj = window.open(url,
+                    "ModalPopUp",
+                    "popup=yes," +
+                    "toolbar=no," +
+                    "scrollbars=no," +
+                    "location=no," +
+                    "statusbar=no," +
+                    "menubar=no," +
+                    "resizable=0," +
+                    "width=800," +
+                    "height=600," +
+                    "left = 490," +
+                    "top=100");
+
+                popUpObj.focus();
+
+            },
+            */
+
+          startup: function () {
+            this.inherited(arguments);
+            //this.mapIdNode.innerHTML = 'map id:' + this.map.id;
+            var map = this.map;
+            var srMap = map.extent.spatialReference;
+            //console.log(map.extent.spatialReference);
+            console.log('startup');
+
+
+            // coordinateFormatter spatial reference 
+            const geoSpatialReference = new SpatialReference({
+              wkid: 4326
+            });
+
+            var redSpatialReference = new SpatialReference({
+              wkid: 3395 //spatial reference of 500K rasters
+            });
+
+            var message = document.getElementById("message");
+            //var messagestatus = document.getElementById("messagestatus");
+            //var messageaddstatus = document.getElementById("messageaddstatus");
+
+            //var redsym = new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([255, 0, 0, 0.5]), 3);
+            //var redsymdd = new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT, new Color([255, 0, 0, 0.5]), 3);
+            var sfsPoly = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+              new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT,
+                new Color([255, 0, 0]), 3), new Color([255, 255, 0, 0.1]));
+
+            this.drawtoolbar = new Draw(this.map)
+            this.drawtoolbar.on("draw-end", this.addToMap)
+            //this.drawtoolbar.on("draw-start", this.startDraw)
+
+          },
+
+
+      // onOpen: function(){
+      //   console.log('onOpen');
+      // },
+
+      // onClose: function(){
+      //   console.log('onClose');
+      // },
+
+      // onMinimize: function(){
+      //   console.log('onMinimize');
+      // },
+
+      // onMaximize: function(){
+      //   console.log('onMaximize');
+      // },
+
+      // onSignIn: function(credential){
+      //   /* jshint unused:false*/
+      //   console.log('onSignIn');
+      // },
+
+      // onSignOut: function(){
+      //   console.log('onSignOut');
+      // }
+
+      // onPositionChange: function(){
+      //   console.log('onPositionChange');
+      // },
+
+      // resize: function(){
+      //   console.log('resize');
+      // }
+
+
+      //methods to communication between widgets:
+
+    });
+  });
---
+++ XOtgSearch/setting/Setting.js
@@ -0,0 +1,106 @@
+///////////////////////////////////////////////////////////////////////////
+// Copyright © Esri. All Rights Reserved.
+//
+// Licensed under the Apache License Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///////////////////////////////////////////////////////////////////////////
+
+
+define(["dojo/_base/declare",
+  "dojo/_base/lang",
+  "dojo/query",
+  "dojo/dom-class",
+  "dojo/dom-construct",
+  "jimu/BaseWidgetSetting",
+  "dijit/_WidgetsInTemplateMixin",
+  "dijit/form/Form",
+  "jimu/dijit/CheckBox",
+  "dijit/form/NumberTextBox",
+  "dijit/form/ValidationTextBox"
+],
+  function (declare, lang, query, domClass, domConstruct,
+    BaseWidgetSetting, _WidgetsInTemplateMixin) {
+
+    return declare([BaseWidgetSetting, _WidgetsInTemplateMixin], {
+      baseClass: 'jimu-widget-xotgparcel-setting',
+
+
+      defaultDeltaXMercator: 0,
+      defaultDeltaYMercator: 0,
+      defaultDeltaX: 0,
+      defaultDeltaY: 0,
+      postCreate: function () {
+        //the config object is passed in
+
+        //this.defaultDeltaY = this.config.defaultDeltaY;
+        this.setConfig(this.config);
+        //The jimu/dijit/CheckBox does not have an onClick event. 
+        this.isBaseMercator.onChange = lang.hitch(this, this._onIsBaseMercatorChange);
+      },
+
+
+      setConfig: function (config) {
+        //console.log(config.UrlServiceGP);
+        //this.textNode.value = config.configText;
+        this.defaultDeltaXMercator = config.defaultDeltaXMercator;
+        this.defaultDeltaYMercator = config.defaultDeltaYMercator;
+        this.defaultDeltaX = config.defaultDeltaX;
+        this.defaultDeltaY = config.defaultDeltaY;
+
+        this.UrlServiceGP.value = config.UrlServiceGP;
+        this.fileUploadUrl.value = config.fileUploadUrl;
+        this.deltaX.value = config.deltaX;
+        this.deltaY.value = config.deltaY;
+          this.isBaseMercator.setValue(config.isBaseMercator);
+
+          
+        //alert(this.isBaseMercator.checked)
+      },
+
+      getConfig: function () {
+        //WAB will get config object through this method
+        console.log("getconf");
+        //alert(this.isBaseMercator.checked)
+        return {
+          //configText: this.textNode.value
+          UrlServiceGP: this.UrlServiceGP.value
+          , fileUploadUrl: this.fileUploadUrl.value
+          , deltaX: this.deltaX.value
+          , deltaY: this.deltaY.value
+          , isBaseMercator: this.isBaseMercator.getValue()
+          , defaultDeltaXMercator: this.defaultDeltaXMercator
+          , defaultDeltaYMercator: this.defaultDeltaYMercator
+          , defaultDeltaX: this.defaultDeltaX
+          , defaultDeltaY: this.defaultDeltaY
+
+        };
+        },
+
+      _onIsBaseMercatorChange: function () {
+        console.log("change isbasemercator");
+
+        if (this.isBaseMercator.checked) {
+          this.deltaX.value = this.defaultDeltaXMercator;
+          this.deltaY.value = this.defaultDeltaYMercator;
+        } else {
+          this.deltaX.value = this.defaultDeltaX;
+          this.deltaY.value = this.defaultDeltaY;
+        }
+        //isBaseMercator = false;
+        //deltaX = 122.4, deltaY = 20.0; 
+      },
+
+      _onBtnValidateClick: function () {
+        //console.log("validate");
+      }
+    });
+  });
