f377956449f8ca428a315a48331704edab744b1b
OlVlas <zsoft.dp@gmail.com>
2023-11-24 15:18:43 +0200

    PolyLine

---
+++ XOtgLine/nls/strings.js
@@ -0,0 +1,11 @@
+﻿define({
+    root: ({
+        _widgetLabel: "XOtgParcel",
+        btnClearText: "Прибрати з карти",
+        headerText: "Накреслити ділянку",
+        buttonPolygonText: "Накреслити ділянку"
+    }),
+    "ru": 1,
+    "uk": 1,
+    "en": 1
+});
---
+++ XOtgLine/setting/nls/ru/strings.js
@@ -0,0 +1,10 @@
+define({
+	root: ({
+		configText: "Текст: ",
+		UrlServiceGP: "URL сервиса геообработки",
+		fileUploadUrl: "URL точки загрузки файла",
+		meters: "метров",
+		isBaseMercator: "Проекция базовой карты Mercator"
+
+	})
+});
